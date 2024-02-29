200a20c572997010abdf0a2078db50492b6c1f90
priel gozlan <prielgozlan@gmail.com>
2024-02-29 20:24:33 +0200

    priel

--- Cash_register.py
+++ Cash_register.py
@@ -45,84 +45,62 @@ def open_new_window():
         widget.destroy()
     display = tk.Entry(root, width=0, font=('Arial', 20))
     display.grid(row=0, column=0, columnspan=5, sticky='nsew')
+    con_1 = 1
+    con_2 = 0
+    con_3 = 0
+    list_1 = ["פיצהL", "סלט", "מאפה", "פיצהXL", "טוסט", "פלאפל", "פיצהXXL", "לאפה", "פיתה"]
+    for item in range(3):
+        for item1 in range(3):
+            bun1 = tk.Button(root, text=list_1[con_3],
+                             width=10, height=10, command=lambda: change_price(list_1[con_3]))
+            bun1.grid(row=con_1, column=con_2, sticky="nsew")
+            con_1 += 1
+            con_3 += 1
+        con_2 += 1
+        con_1 = 1
     but_1 = tk.Button(root, text="שינוי מחיר",
                       width=10, height=10, command=lambda: display.insert(0, "בחר מוצר :"))
     but_1.grid(row=3, column=3, sticky="nsew")
     but_2 = tk.Button(root, text=" חזור",
-                      width=10, height=10)
+                      width=10, height=10, command=button)
     but_2.grid(row=1, column=3, sticky="nsew")
     but_3 = tk.Button(root, text="שינוי שם",
                       width=10, height=10)
     but_3.grid(row=2, column=3, sticky="nsew")
-    bun1 = tk.Button(root, text="פיצהL",
-                     width=10, height=10, command=lambda: change_price("פיצהL"))
-    bun1.grid(row=1, column=0, sticky="nsew")
-    bun2 = tk.Button(root, text="מאפה",
-                     width=10, height=10)
-    bun2.grid(row=2, column=0, sticky="nsew")
-    bun3 = tk.Button(root, text="סלט",
-                     width=10, height=10)
-    bun3.grid(row=3, column=0, sticky="nsew")
-    bun4 = tk.Button(root, text="פלאפל",
-                     width=10, height=10)
-    bun4.grid(row=2, column=1, sticky="nsew")
-    bun5 = tk.Button(root, text="פיצהXL",
-                     width=10, height=10)
-    bun5.grid(row=1, column=1, sticky="nsew")
-    bun6 = tk.Button(root, text="פיצהXXL",
-                     width=10, height=10)
-    bun6.grid(row=1, column=2, sticky="nsew")
-    bun7 = tk.Button(root, text="טוסט",
-                     width=10, height=10)
-    bun7.grid(row=3, column=1, sticky="nsew")
-    bun8 = tk.Button(root, text="לאפה",
-                     width=10, height=10)
-    bun8.grid(row=3, column=2, sticky="nsew")
-    bun9 = tk.Button(root, text="פיתה",
-                     width=10, height=10)
-    bun9.grid(row=2, column=2, sticky="nsew")
 
     def change_price(product):
+        print(product)
+
         def on_enter(event):
             user_input = display.get()
-            print(user_input)
-            for item in list_of_product.keys():
-                if item == product:
+            # print(user_input)
+            for z in list_of_product.keys():
+                if z == product:
                     list_of_product[item] = int(user_input)
                     print(list_of_product[item])
+                else:
+                    print("error")
+                    print(product)
 
         display.delete(0, tk.END)
         display.bind("<Return>", on_enter)
 
 
 def button():
-    bun1 = tk.Button(root, text="פיצהL",
-                     width=10, height=10, command=lambda: add_product("פיצהL"))
-    bun1.grid(row=1, column=0, sticky="nsew")
-    bun2 = tk.Button(root, text="מאפה",
-                     width=10, height=10, command=lambda: add_product("מאפה"))
-    bun2.grid(row=2, column=0, sticky="nsew")
-    bun3 = tk.Button(root, text="סלט",
-                     width=10, height=10, command=lambda: add_product("סלט"))
-    bun3.grid(row=3, column=0, sticky="nsew")
-    bun4 = tk.Button(root, text="פלאפל",
-                     width=10, height=10, command=lambda: add_product("פלאפל"))
-    bun4.grid(row=2, column=1, sticky="nsew")
-    bun5 = tk.Button(root, text="פיצהXL",
-                     width=10, height=10, command=lambda: add_product("פיצהXL"))
-    bun5.grid(row=1, column=1, sticky="nsew")
-    bun6 = tk.Button(root, text="פיצהXXL",
-                     width=10, height=10, command=lambda: add_product("פיצהXXL"))
-    bun6.grid(row=1, column=2, sticky="nsew")
-    bun7 = tk.Button(root, text="טוסט",
-                     width=10, height=10, command=lambda: add_product("טוסט"))
-    bun7.grid(row=3, column=1, sticky="nsew")
-    bun8 = tk.Button(root, text="לאפה",
-                     width=10, height=10, command=lambda: add_product("לאפה"))
-    bun8.grid(row=3, column=2, sticky="nsew")
-    bun9 = tk.Button(root, text="פיתה",
-                     width=10, height=10, command=lambda: add_product("פיתה"))
-    bun9.grid(row=2, column=2, sticky="nsew")
+    con_1 = 1
+    con_2 = 0
+    con_3 = 0
+    list_1 = ["פיצהL", "סלט", "מאפה", "פיצהXL", "טוסט", "פלאפל", "פיצהXXL", "לאפה", "פיתה"]
+    for item in range(3):
+        for item1 in range(3):
+            bun1 = tk.Button(root, text=list_1[con_3],
+                             width=10, height=10, command=lambda: add_product(list_1[con_3]))
+            bun1.grid(row=con_1, column=con_2, sticky="nsew")
+            con_1 += 1
+            con_3 += 1
+        con_2 += 1
+        con_1 = 1
+
     bun10 = tk.Button(root, text="חשבון חדש",
                       width=10, height=10, command=lambda: delete_sum_all())
     bun10.grid(row=3, column=3, sticky="nsew")
@@ -138,3 +116,61 @@ def button():
 button()
 
 root.mainloop()
+
+# לחצים גירסה א'
+# bun1 = tk.Button(root, text="פיצהL",
+#                  width=10, height=10, command=lambda: add_product("פיצהL"))
+# bun1.grid(row=1, column=0, sticky="nsew")
+# bun2 = tk.Button(root, text="מאפה",
+#                  width=10, height=10, command=lambda: add_product("מאפה"))
+# bun2.grid(row=2, column=0, sticky="nsew")
+# bun3 = tk.Button(root, text="סלט",
+#                  width=10, height=10, command=lambda: add_product("סלט"))
+# bun3.grid(row=3, column=0, sticky="nsew")
+# bun4 = tk.Button(root, text="פיצהXL",
+#                  width=10, height=10, command=lambda: add_product("פיצהXL"))
+# bun4.grid(row=1, column=1, sticky="nsew")
+# bun5 = tk.Button(root, text="טוסט",
+#                  width=10, height=10, command=lambda: add_product("טוסט"))
+# bun5.grid(row=2, column=1, sticky="nsew")
+# bun6 = tk.Button(root, text="פלאפל",
+#                  width=10, height=10, command=lambda: add_product("פלאפל"))
+# bun6.grid(row=3, column=1, sticky="nsew")
+# bun7 = tk.Button(root, text="פיצהXXL",
+#                  width=10, height=10, command=lambda: add_product("פיצהXXL"))
+# bun7.grid(row=1, column=2, sticky="nsew")
+# bun8 = tk.Button(root, text="לאפה",
+#                  width=10, height=10, command=lambda: add_product("לאפה"))
+# bun8.grid(row=2, column=2, sticky="nsew")
+# bun9 = tk.Button(root, text="פיתה",
+#                  width=10, height=10, command=lambda: add_product("פיתה"))
+# bun9.grid(row=3, column=2, sticky="nsew")
+
+
+# bun1 = tk.Button(root, text="פיצהL",
+#                  width=10, height=10, command=lambda: change_price("פיצהL"))
+# bun1.grid(row=1, column=0, sticky="nsew")
+# bun2 = tk.Button(root, text="מאפה",
+#                  width=10, height=10, command=lambda: change_price("מאפה"))
+# bun2.grid(row=2, column=0, sticky="nsew")
+# bun3 = tk.Button(root, text="סלט",
+#                  width=10, height=10, command=lambda: change_price("סלט"))
+# bun3.grid(row=3, column=0, sticky="nsew")
+# bun4 = tk.Button(root, text="פלאפל",
+#                  width=10, height=10, command=lambda: change_price("פלאפל"))
+# bun4.grid(row=2, column=1, sticky="nsew")
+# bun5 = tk.Button(root, text="פיצהXL",
+#                  width=10, height=10, command=lambda: change_price("פיצהXL"))
+# bun5.grid(row=1, column=1, sticky="nsew")
+# bun6 = tk.Button(root, text="פיצהXXL",
+#                  width=10, height=10, command=lambda: change_price("פיצהXXL"))
+# bun6.grid(row=1, column=2, sticky="nsew")
+# bun7 = tk.Button(root, text="טוסט",
+#                  width=10, height=10, command=lambda: change_price("טוסט"))
+# bun7.grid(row=3, column=1, sticky="nsew")
+# bun8 = tk.Button(root, text="לאפה",
+#                  width=10, height=10, command=lambda: change_price("לאפה"))
+# bun8.grid(row=3, column=2, sticky="nsew")
+# bun9 = tk.Button(root, text="פיתה",
+#                  width=10, height=10, command=lambda: change_price("פיתה"))
+# bun9.grid(row=2, column=2, sticky="nsew")
