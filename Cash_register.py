55852ba90918b2a513077b6f85dd026301b2d0c6
priel gozlan <prielgozlan@gmail.com>
2024-03-10 17:36:33 +0200

    priel

--- Cash_register.py
+++ Cash_register.py
@@ -25,6 +25,8 @@ def add_product(key_1):
     elif x == "subtract":
         sum_all -= list_of_product[key_1]
         x = "add"
+        if sum_all < 0:
+            sum_all = 0
     display.insert(0, str(sum_all))
     print(list_of_product[key_1])
 
@@ -89,19 +91,46 @@ def open_new_window():
 
 
 def button():
-    con_1 = 1
-    con_2 = 0
-    con_3 = 0
-    list_1 = ["פיצהL", "סלט", "מאפה", "פיצהXL", "טוסט", "פלאפל", "פיצהXXL", "לאפה", "פיתה"]
-    for item in range(3):
-        for item1 in range(3):
-            bun1 = tk.Button(root, text=list_1[con_3],
-                             width=10, height=10, command=lambda: add_product(list_1[con_3]))
-            bun1.grid(row=con_1, column=con_2, sticky="nsew")
-            con_1 += 1
-            con_3 += 1
-        con_2 += 1
-        con_1 = 1
+    # con_1 = 1
+    # con_2 = 0
+    # con_3 = 0
+    # list_1 = ["פיצהL", "סלט", "מאפה", "פיצהXL", "טוסט", "פלאפל", "פיצהXXL", "לאפה", "פיתה"]
+    # for item in range(3):
+    #     for item1 in range(3):
+    #         bun1 = tk.Button(root, text=list_1[con_3],
+    #                          width=10, height=10, command=lambda: add_product(list_1[con_3]))
+    #         bun1.grid(row=con_1, column=con_2, sticky="nsew")
+    #         con_1 += 1
+    #         con_3 += 1
+    #     con_2 += 1
+    #     con_1 = 1
+    bun1 = tk.Button(root, text="פיצהL",
+                     width=10, height=10, command=lambda: add_product("פיצהL"))
+    bun1.grid(row=1, column=0, sticky="nsew")
+    bun2 = tk.Button(root, text="מאפה",
+                     width=10, height=10, command=lambda: add_product("מאפה"))
+    bun2.grid(row=2, column=0, sticky="nsew")
+    bun3 = tk.Button(root, text="סלט",
+                     width=10, height=10, command=lambda: add_product("סלט"))
+    bun3.grid(row=3, column=0, sticky="nsew")
+    bun4 = tk.Button(root, text="פיצהXL",
+                     width=10, height=10, command=lambda: add_product("פיצהXL"))
+    bun4.grid(row=1, column=1, sticky="nsew")
+    bun5 = tk.Button(root, text="טוסט",
+                     width=10, height=10, command=lambda: add_product("טוסט"))
+    bun5.grid(row=2, column=1, sticky="nsew")
+    bun6 = tk.Button(root, text="פלאפל",
+                     width=10, height=10, command=lambda: add_product("פלאפל"))
+    bun6.grid(row=3, column=1, sticky="nsew")
+    bun7 = tk.Button(root, text="פיצהXXL",
+                     width=10, height=10, command=lambda: add_product("פיצהXXL"))
+    bun7.grid(row=1, column=2, sticky="nsew")
+    bun8 = tk.Button(root, text="לאפה",
+                     width=10, height=10, command=lambda: add_product("לאפה"))
+    bun8.grid(row=2, column=2, sticky="nsew")
+    bun9 = tk.Button(root, text="פיתה",
+                     width=10, height=10, command=lambda: add_product("פיתה"))
+    bun9.grid(row=3, column=2, sticky="nsew")
 
     bun10 = tk.Button(root, text="חשבון חדש",
                       width=10, height=10, command=lambda: delete_sum_all())
